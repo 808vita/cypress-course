b4d5489cdf4e54db5555e3007c74ce861477c6a9
808vita <its808vita@gmail.com>
2024-01-19 22:38:38 +0530

    alias - input alias

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -6,8 +6,15 @@ describe("form tests", () => {
   it("test subscribe form", () => {
     cy.contains(/testing forms/i);
     cy.getDataTest("subscribe-form").find("input").type("oof@oof.com");
-    cy.contains(/Successfully subbed: ryan@coderyan.com!/i).should("not.exist");
+
+    cy.getDataTest("subscribe-form").find("input").as("subscribe-input");
+    cy.get("@subscribe-input").type("oof@oof.com");
+
     cy.contains(/Successfully subbed: /i).should("not.exist");
     cy.getDataTest("subscribe-button").click();
+
+    cy.contains(/Successfully subbed: /i).should("exist");
+    cy.wait(3000);
+    cy.contains(/Successfully subbed: /i).should("not.exist");
   });
 });
