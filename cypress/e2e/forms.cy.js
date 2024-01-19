238760218673a64db74a23f6ef8086b224c841a2
808vita <its808vita@gmail.com>
2024-01-19 23:14:54 +0530

    invalid & empty input cases

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -5,7 +5,7 @@ describe("form tests", () => {
 
   it("test subscribe form", () => {
     cy.contains(/testing forms/i);
-    cy.getDataTest("subscribe-form").find("input").type("oof@oof.com");
+    // cy.getDataTest("subscribe-form").find("input").type("oof@oof.com");
 
     cy.getDataTest("subscribe-form").find("input").as("subscribe-input");
     cy.get("@subscribe-input").type("oof@oof.com");
@@ -16,5 +16,16 @@ describe("form tests", () => {
     cy.contains(/Successfully subbed: /i).should("exist");
     cy.wait(3000);
     cy.contains(/Successfully subbed: /i).should("not.exist");
+
+    cy.get("@subscribe-input").type("oof@oof.oof");
+    cy.contains(/invalid email: oof@oof.oof!/i).should("not.exist");
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/invalid email: oof@oof.oof!/i).should("exist");
+    cy.wait(3000);
+    cy.contains(/invalid email: oof@oof.oof!/i).should("not.exist");
+
+    cy.contains(/fail/i).should("not.exist");
+    cy.getDataTest("subscribe-button").click();
+    cy.contains(/fail/i).should("exits");
   });
 });
